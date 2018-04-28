57b56c14322972f35ee3db9acc55324efb84f545
ciruss <johanlaas4@gmail.com>
2018-04-28 20:28:34 +0300

    another cleanUp

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -25,18 +25,7 @@ class Personal extends Component {
 					<dt class="dib b"><i className="material-icons">call</i></dt>
 					<dd class="dib">56662971</dd>
 				</dl>
-			</div>/* ,
-			<div className='personal'>
-				<p><i className="material-icons">person</i>Johan Laas</p>
-				<p><i className="material-icons">cake</i>24.06.1991</p>
-				<p>
-					<a class='link underline-hover' href={'mailto:' + 'johanlaas4@gmail.com'} style={{color: 'black'}}>
-						<i className="material-icons">mail</i>
-						johanlaas4@gmail.com
-					</a>
-				</p>
-				<p><i className="material-icons">call</i>56662971</p>
-			</div> */
+			</div>
 		)
 	}
 }
